ced825b9a2d6475f2e72d0fb8b7ef0ce33a7d242
Thijs <thijsvandiessen@gmail.com>
2024-06-08 16:50:59 +0200

    fix: add files path

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -2,13 +2,13 @@ resource "vercel_project" "lundi-bleu" {
   name = "lundi-bleu"
 }
 
-resource "vercel_deployment" "lundi-bleu" {
-  project_id = vercel_project.lundi-bleu.id
-  ref        = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
-  production = false
-}
-
 data "vercel_project_directory" "lundi-bleu" {
   path = "../apps/lundi-bleu"
 }
 
+resource "vercel_deployment" "lundi-bleu" {
+  project_id = vercel_project.lundi-bleu.id
+  files      = data.vercel_project_directory.lundi-bleu.path
+  ref        = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
+  production = false
+}
