5aa4947fb679d5fc5a0287aed238c9a252d5859a
Thijs <thijsvandiessen@users.noreply.github.com>
2024-06-08 16:36:29 +0200

    Feat/vercel provider config (#13)
    * feat: vercel provider config
    
    * feat: lundi bleu terraform project
    
    * feat: deployment to vercel

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -1,3 +1,12 @@
 resource "vercel_project" "lundi-bleu" {
   name = "lundi-bleu"
 }
+
+resource "vercel_deployment" "lundi-bleu" {
+  project_id = vercel_project.lundi-bleu.id
+}
+
+data "vercel_project_directory" "lundi-bleu" {
+  path = "../apps/lundi-bleu"
+}
+
