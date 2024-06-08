81f09344d43048b13fc87cc1392afcbd54f568fd
Thijs <thijsvandiessen@gmail.com>
2024-06-08 17:01:34 +0200

    fix: no ref if files

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -10,7 +10,6 @@ resource "vercel_deployment" "lundi" {
   project_id  = data.vercel_project.lundi.id
   files       = data.vercel_project_directory.lundi.files
   path_prefix = data.vercel_project_directory.lundi.path
-  ref         = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
   production  = true
 
   environment = {
