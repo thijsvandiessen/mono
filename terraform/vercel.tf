fc1596af81af0a4acb0f8f6b909b89bbd8980310
Thijs <thijsvandiessen@gmail.com>
2024-06-08 17:19:45 +0200

    fix: try

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -1,18 +1,14 @@
-data "vercel_project" "lundi" {
+data "vercel_project" "lundi_bleu" {
   name = "lundi-bleu"
 }
 
-data "vercel_project_directory" "lundi" {
+data "vercel_project_directory" "lundi_bleu" {
   path = "../apps/lundi-bleu"
 }
 
-resource "vercel_deployment" "lundi" {
-  project_id  = data.vercel_project.lundi.id
-  files       = data.vercel_project_directory.lundi.files
-  path_prefix = data.vercel_project_directory.lundi.path
+resource "vercel_deployment" "lundi_bleu" {
+  project_id  = data.vercel_project.lundi_bleu.id
+  files       = data.vercel_project_directory.lundi_bleu.files
+  path_prefix = data.vercel_project_directory.lundi_bleu.path
   production  = true
-
-  environment = {
-    FOO = "bar"
-  }
 }
