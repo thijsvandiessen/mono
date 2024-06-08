e9084da29b1c6ca05999c7473245fa805dc9c410
Thijs <thijsvandiessen@users.noreply.github.com>
2024-06-08 17:11:24 +0200

    Fix/terraform vercel deployment ref (#16)
    * fix: deployment-ref
    
    * fix: production tag
    
    * fix: broken function
    
    * fix: add files path
    
    * fix: add missing properties
    
    * fix: no ref if files
    
    * fix: name
    
    * fix: typo
    
    * fix: typo

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -1,14 +1,18 @@
-resource "vercel_project" "lundi-bleu" {
+data "vercel_project" "lundi" {
   name = "lundi-bleu"
 }
 
-data "vercel_project_directory" "lundi-bleu" {
+data "vercel_project_directory" "lundi" {
   path = "../apps/lundi-bleu"
 }
 
-resource "vercel_deployment" "lundi-bleu" {
-  project_id = vercel_project.lundi-bleu.id
-  files      = data.vercel_project_directory.lundi-bleu.path
-  ref        = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
-  production = false
+resource "vercel_deployment" "lundi" {
+  project_id  = data.vercel_project.lundi.id
+  files       = data.vercel_project_directory.lundi.files
+  path_prefix = data.vercel_project_directory.lundi.path
+  production  = true
+
+  environment = {
+    FOO = "bar"
+  }
 }
