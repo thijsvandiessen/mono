c7b024ba0aaea20b19ac446fffb9b2a2152ae5d6
Thijs <thijsvandiessen@gmail.com>
2024-06-08 17:30:30 +0200

    fix: test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,6 +11,11 @@ variable "vercel_api_token" {
   default   = ""
 }
 
+variable "vercel_team_id" {
+  sensitive = false
+  type      = string
+}
+
 variable "TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA" {
   type = string
 }
--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -2,13 +2,13 @@ data "vercel_project" "lundi" {
   name = "lundi"
 }
 
-# data "vercel_project_directory" "lundi" {
-#   path = "../apps/lundi-bleu"
-# }
+data "vercel_project_directory" "lundi" {
+  path = "../apps/lundi-bleu"
+}
 
-# resource "vercel_deployment" "lundi" {
-#   project_id  = data.vercel_project.lundi.id
-#   files       = data.vercel_project_directory.lundi.files
-#   path_prefix = data.vercel_project_directory.lundi.path
-#   production  = true
-# }
+resource "vercel_deployment" "lundi" {
+  project_id  = data.vercel_project.lundi.id
+  files       = data.vercel_project_directory.lundi.files
+  path_prefix = data.vercel_project_directory.lundi.path
+  production  = true
+}
