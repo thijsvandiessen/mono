c9d588b0e4836669a67020aa478c3652590ea71c
Thijs <thijsvandiessen@gmail.com>
2024-06-08 16:57:57 +0200

    fix: add missing properties

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -1,14 +1,19 @@
-resource "vercel_project" "lundi-bleu" {
-  name = "lundi-bleu"
+resource "vercel_project" "lundi" {
+  name = "lundi"
 }
 
-data "vercel_project_directory" "lundi-bleu" {
-  path = "../apps/lundi-bleu"
+data "vercel_project_directory" "lundi" {
+  path = "../apps/lundi"
 }
 
-resource "vercel_deployment" "lundi-bleu" {
-  project_id = vercel_project.lundi-bleu.id
-  files      = data.vercel_project_directory.lundi-bleu.path
-  ref        = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
-  production = false
+resource "vercel_deployment" "lundi" {
+  project_id  = data.vercel_project.lundi.id
+  files       = data.vercel_project_directory.lundi.files
+  path_prefix = data.vercel_project_directory.lundi.path
+  ref         = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
+  production  = true
+
+  environment = {
+    FOO = "bar"
+  }
 }
