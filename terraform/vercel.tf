5607327eb8de7468744d55ce7397c6c83c213ba0
Thijs <thijsvandiessen@gmail.com>
2024-06-08 17:22:23 +0200

    fix: comment

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -2,13 +2,13 @@ data "vercel_project" "lundi_bleu" {
   name = "lundi-bleu"
 }
 
-data "vercel_project_directory" "lundi_bleu" {
-  path = "../apps/lundi-bleu"
-}
+# data "vercel_project_directory" "lundi_bleu" {
+#   path = "../apps/lundi-bleu"
+# }
 
-resource "vercel_deployment" "lundi_bleu" {
-  project_id  = data.vercel_project.lundi_bleu.id
-  files       = data.vercel_project_directory.lundi_bleu.files
-  path_prefix = data.vercel_project_directory.lundi_bleu.path
-  production  = true
-}
+# resource "vercel_deployment" "lundi_bleu" {
+#   project_id  = data.vercel_project.lundi_bleu.id
+#   files       = data.vercel_project_directory.lundi_bleu.files
+#   path_prefix = data.vercel_project_directory.lundi_bleu.path
+#   production  = true
+# }
