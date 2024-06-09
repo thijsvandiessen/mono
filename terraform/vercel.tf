87e614755a9db7f26a6900bc79c1ca11b86f9c05
Thijs <thijsvandiessen@gmail.com>
2024-06-09 22:29:34 +0200

    fix: setup-vercel

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -1,20 +1,3 @@
 resource "vercel_project" "lundi" {
   name = "lundi"
 }
-
-data "vercel_project_directory" "lundi" {
-  path = "../"
-}
-
-resource "vercel_deployment" "lundi" {
-  project_id  = resource.vercel_project.lundi.id
-  files       = data.vercel_project_directory.lundi.files
-  path_prefix = data.vercel_project_directory.lundi.path
-  production  = true
-  project_settings = {
-    framework       = "nextjs"
-    install_command = "npm ci"
-    build_command   = "npm run build"
-    root_directory  = "apps/lundi-bleu"
-  }
-}
