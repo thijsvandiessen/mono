dae44659d2d14b128b51d449be38a551cc27d66e
Thijs <thijsvandiessen@gmail.com>
2024-06-10 00:23:08 +0200

    fix: fix all

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -1,3 +1,19 @@
 resource "vercel_project" "lundi" {
   name = "lundi"
 }
+
+data "vercel_project_directory" "lundi" {
+  path = "../"
+}
+
+resource "vercel_deployment" "lundi" {
+  project_id  = resource.vercel_project.lundi.id
+  files       = data.vercel_project_directory.lundi.files
+  path_prefix = data.vercel_project_directory.lundi.path
+  production  = true
+  project_settings = {
+    install_command  = "npm ci"
+    build_command    = "npm run build"
+    output_directory = "apps/lundi-bleu/.next"
+  }
+}
