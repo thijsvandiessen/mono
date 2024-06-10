2cefbe20170025d6dc368f6a9bb42bd23ab70305
Thijs <thijsvandiessen@users.noreply.github.com>
2024-06-10 14:14:33 +0200

    Fix/env vars (#23)
    * fix: env vars lundi
    
    * fix: setup-vercel
    
    * fix: only deploy lundi
    
    * fix: deps mismatch
    
    * fix: mismatch
    
    * fix: errors
    
    * fix: bring back client
    
    * fix: fix all
    
    * fix: test
    
    * fix: linting

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -5,15 +5,14 @@ resource "vercel_project" "lundi" {
 data "vercel_project_directory" "lundi" {
   path = "../"
 }
-
 resource "vercel_deployment" "lundi" {
   project_id  = resource.vercel_project.lundi.id
   files       = data.vercel_project_directory.lundi.files
   path_prefix = data.vercel_project_directory.lundi.path
   production  = true
   project_settings = {
-    install_command  = "npm ci"
-    build_command    = "npm run build"
-    output_directory = "apps/lundi-bleu/.next"
+    install_command = "npm ci"
+    build_command   = "npm run build"
+    root_directory  = "apps/lundi-bleu"
   }
 }
