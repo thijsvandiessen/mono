5a64130c103ba50015da03c6636148f63ccbc7fc
Thijs <thijsvandiessen@gmail.com>
2024-08-16 12:16:51 +0200

    fix: commitlint rule

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -1,6 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [2, 'always', 720],
+    'body-max-line-length': [2, 'always', 1000],
+    'footer-max-line-length': [2, 'always', 1000],
   },
 }
