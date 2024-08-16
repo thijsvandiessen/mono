ae9161c370cadeb21b064b7c67b566e30af6fd76
Thijs <thijsvandiessen@gmail.com>
2024-08-16 12:12:44 +0200

    fix: retry options

--- packages/graphql/src/gqlClient.ts
+++ packages/graphql/src/gqlClient.ts
@@ -1,11 +1,14 @@
 import cacheExchange from './cacheExchange'
 import { linkConfig } from './linkConfig'
 import { Client, fetchExchange, mapExchange } from '@urql/core'
+import { retryExchange } from '@urql/exchange-retry'
+import { retryExchangeOptions } from './retryExchange'
 
 export const client = new Client({
   url: linkConfig.url,
   exchanges: [
     cacheExchange,
+    retryExchange(retryExchangeOptions),
     mapExchange({
       onError(error) {
         console.log(error, error.graphQLErrors[0]?.extensions)
