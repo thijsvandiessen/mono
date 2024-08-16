64435d8f78749bb1ae611f7cd16a0ad4224ddb48
Thijs <thijsvandiessen@gmail.com>
2024-08-16 11:33:50 +0200

    fix: linting

--- packages/utils/src/isValidUrl.ts
+++ packages/utils/src/isValidUrl.ts
@@ -3,6 +3,9 @@ export const isValidUrl = (url: string): boolean => {
     // TODO: edge case when url should be relative
     return Boolean(new URL(url))
   } catch (e) {
+    if (e instanceof TypeError) {
+      console.log(`Invalid URL': ${e.message}`)
+    }
     return false
   }
 }
