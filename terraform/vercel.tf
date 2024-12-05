e41a745d0240f3e6230f5864fc94bdca8840ffa2
Thijs <thijsvandiessen@gmail.com>
2024-12-05 15:18:45 +0100

    fix: update terraform config vercel

--- terraform/vercel.tf
+++ terraform/vercel.tf
@@ -24,20 +24,42 @@ resource "vercel_deployment" "example" {
   project_id  = resource.vercel_project.example.id
   files       = data.vercel_project_directory.mono.files
   path_prefix = data.vercel_project_directory.mono.path
+  ref         = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
   production  = var.TFC_CONFIGURATION_VERSION_GIT_TAG != "" ? true : false
+}
+
+resource "vercel_project_deployment_retention" "example" {
+  project_id            = vercel_project.example.id
+  team_id               = vercel_project.example.team_id
+  expiration_preview    = "3m"
+  expiration_production = "unlimited"
+  expiration_canceled   = "1m"
+  expiration_errored    = "1m"
 }
 
 resource "vercel_deployment" "lundi" {
   project_id  = resource.vercel_project.lundi.id
   files       = data.vercel_project_directory.mono.files
   path_prefix = data.vercel_project_directory.mono.path
+  ref         = var.TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA
   production  = var.TFC_CONFIGURATION_VERSION_GIT_TAG != "" ? true : false
 }
 
-resource "vercel_project_environment_variable" "lundi" {
+resource "vercel_project_deployment_retention" "lundi" {
+  project_id            = vercel_project.lundi.id
+  team_id               = vercel_project.lundi.team_id
+  expiration_preview    = "3m"
+  expiration_production = "unlimited"
+  expiration_canceled   = "1m"
+  expiration_errored    = "1m"
+}
+
+resource "vercel_project_environment_variables" "lundi" {
   project_id = vercel_project.lundi.id
-  key        = "NEXT_PUBLIC_GOOGLE_MAPS_API_KEY"
-  value      = var.GOOGLE_MAPS_API_KEY
-  target     = ["preview", "production"]
-  sensitive  = false
+  variables = [{
+    key       = "NEXT_PUBLIC_GOOGLE_MAPS_API_KEY"
+    value     = var.GOOGLE_MAPS_API_KEY
+    target    = ["preview", "production"]
+    sensitive = false
+  }]
 }
