33c25a95a8b2f72d842e0d4238273b576f220c85
Thijs <thijsvandiessen@gmail.com>
2024-12-23 09:36:46 +0100

    fix: updates and scrips

--- apps/lundi-bleu/src/app/[...slug]/page.tsx
+++ apps/lundi-bleu/src/app/[...slug]/page.tsx
@@ -4,14 +4,17 @@ import type { Metadata, Viewport } from 'next'
 import { viewport } from '@mono/utils'
 
 interface PageProps {
-  params: { slug: string[] }
-  searchParams: { [key: string]: string[] | undefined }
+  params: Promise<{ slug: string }>
+  searchParams: Promise<{ [key: string]: string[] | undefined }>
 }
 
 export async function generateMetadata({
   params,
 }: PageProps): Promise<Metadata> {
-  const { data } = await getPageSeo({ slug: params.slug.join('/') })
+  const { slug } = await params
+
+  console.log(slug)
+  const { data } = await getPageSeo({ slug })
   return data
 }
 
@@ -19,8 +22,9 @@ export function generateViewport(): Viewport {
   return viewport
 }
 
-const Page = ({ params: { slug } }: PageProps) => {
-  return <DefaultPage slug={slug.join('/')} />
+const Page = async ({ params }: PageProps) => {
+  const { slug } = await params
+  return <DefaultPage slug={slug} />
 }
 
 export default Page
--- packages/datocms/src/index.ts
+++ packages/datocms/src/index.ts
@@ -1,19 +1,17 @@
-import { buildClient } from '@datocms/cma-client-node'
+import { buildClient, LogLevel } from '@datocms/cma-client-node'
+import { downloadModels } from './downloadModels'
 
-async function run() {
+export async function run() {
   if (process.env.FULL_ACCESS_API_TOKEN === undefined) {
     throw new Error('FULL_ACCESS_API_TOKEN is required')
   }
 
   const client = buildClient({
     apiToken: process.env.FULL_ACCESS_API_TOKEN,
+    logLevel: LogLevel.BASIC,
   })
 
-  const itemTypes = await client.itemTypes.list()
+  await downloadModels(client)
 
-  itemTypes.forEach((itemType) => {
-    console.log(itemType)
-  })
+  console.log('Done!')
 }
-
-run()
