39aa3c778bc5b987d06ec43a1fba2c7f153f4b78
Thijs <thijsvandiessen@gmail.com>
2025-04-19 21:22:21 +0200

    fix: pipeline

--- packages/data/eslint.config.mjs
+++ packages/data/eslint.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 import { eslintConfig } from '@mono/eslint-config'
 
 export default defineConfig([
-  globalIgnores(['**/generated/*.ts', 'dist/*', 'node_modules']),
+  globalIgnores(['src/generated/*.ts', 'dist/*', 'node_modules']),
   eslintConfig,
 ])
