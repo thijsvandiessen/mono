58ebaaf889216af244d42026ee059157e5ea9c02
Thijs <thijsvandiessen@gmail.com>
2025-04-20 11:21:58 +0200

    fix: dts

--- packages/utils/vite.config.ts
+++ packages/utils/vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [
+    dts({
+      rollupTypes: true,
+      tsconfigPath: './tsconfig.json',
+    }),
+  ],
   build: {
     lib: {
       entry: ['./src/index.ts'],
