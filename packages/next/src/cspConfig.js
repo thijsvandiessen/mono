8acd71fc8393083b9dcc9e4c9e7017f8bee8064f
Thijs <thijsvandiessen@gmail.com>
2025-04-20 13:20:09 +0200

    fix: lint order

--- packages/next/src/cspConfig.js
+++ packages/next/src/cspConfig.js
@@ -2,7 +2,7 @@ export const csp = ({ domain }) => [
   `default-src 'self' 'unsafe-inline'`,
   `script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com`,
   `manifest-src ${domain}/manifest.json`, // TODO
-  `img-src 'self' 'unsafe-inline' data: https://maps.gstatic.com https://maps.googleapis.com https://i.ytimg.com https://i.vimeocdn.com`,
+  `img-src 'self' 'unsafe-inline' data: https://maps.gstatic.com https://maps.googleapis.com https://i.ytimg.com https://i.vimeocdn.com https://res.cloudinary.com`,
   `connect-src 'self' https://maps.googleapis.com https://j3j4j0es2k-dsn.algolia.net`,
   `script-src-elem 'self' 'unsafe-inline' https://static.cloudflareinsights.com https://maps.googleapis.com`,
   `style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com`,
