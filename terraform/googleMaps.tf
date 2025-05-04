ae8256efb38289281b4a64d35a7e6eac66bd5893
Thijs <thijsvandiessen@users.noreply.github.com>
2025-05-04 21:30:51 +0200

    fix: explicitly enable (#437)
    both

--- terraform/googleMaps.tf
+++ terraform/googleMaps.tf
@@ -1,11 +1,20 @@
-resource "google_project_service" "project" {
+resource "google_project_service" "iam" {
   project = var.google_project_id
   service = "iam.googleapis.com"
+  
+  disable_on_destroy = false
+}
 
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
+resource "google_project_service" "apikeys" {
+  project = var.google_project_id
+  service = "apikeys.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "maps_backend" {
+  project = var.google_project_id
+  service = "maps-backend.googleapis.com"
 
   disable_on_destroy = false
 }
@@ -14,6 +23,11 @@ resource "google_apikeys_key" "maps" {
   name         = "maps-api-key"
   display_name = "google-maps-api-key"
   project      = var.google_project_id
+
+  depends_on = [
+    google_project_service.apikeys,
+    google_project_service.maps_backend,
+  ]
 
   restrictions {
     api_targets {
@@ -25,7 +39,5 @@ resource "google_apikeys_key" "maps" {
         "https://www.lundibleu.nl/",
       ]
     }
-
   }
 }
-
