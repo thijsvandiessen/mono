d3c288941b38dd9391b52d48cb473cdd65116056
Thijs <thijsvandiessen@gmail.com>
2025-05-21 16:19:59 +0200

    fix: setup playwright

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
-  testDir: './tests',
+  // Look for test files in the "tests" directory, relative to this configuration file.
+  testDir: 'tests',
+
+  // Run all tests in parallel.
+  fullyParallel: true,
+
+  use: {
+    baseURL: 'http://localhost:3000',
+  },
+
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
   ],
+  webServer: {
+    command: 'npm run dev',
+    cwd: '../',
+    url: 'http://localhost:3000',
+    timeout: 30 * 1000, // 30 sec – bump if your build is slow
+  },
 })
--- e2e/tests/homepage.spec.ts
+++ e2e/tests/homepage.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
 test('homepage has correct title', async ({ page }) => {
-  await page.goto('https://example.com/')
-  await expect(page).toHaveTitle(/Example Domain/)
+  await page.goto('/')
+  await expect(page).toHaveTitle('Homepage | Lundi Bleu - Strijkorkest')
 })
