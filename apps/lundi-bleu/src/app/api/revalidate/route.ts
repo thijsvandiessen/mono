cad44e9bf04163b1a5a5a96db453d7e4143e80dd
Thijs <thijsvandiessen@gmail.com>
2025-11-24 21:34:04 +0100

    fix: revalidate success

--- apps/lundi-bleu/src/app/api/revalidate/route.ts
+++ apps/lundi-bleu/src/app/api/revalidate/route.ts
@@ -1,12 +1,21 @@
 import { NextResponse } from 'next/server.js'
 import { revalidateTag } from 'next/cache.js'
 
-export function POST() {
+function revalidate() {
   revalidateTag('site-content', 'max')
+
+  fetch('https://webhooks.datocms.com/lIkDB28WrL/deploy-results', {
+    method: 'POST',
+    body: JSON.stringify({ status: 'success' }),
+  })
+
   return NextResponse.json({ revalidated: true })
 }
 
+export function POST() {
+  return revalidate()
+}
+
 export function GET() {
-  revalidateTag('site-content', 'max')
-  return NextResponse.json({ revalidated: true })
+  return revalidate()
 }
