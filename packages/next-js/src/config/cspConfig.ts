c245f26625ce3d2b69abf120268a1487b754c560
Thijs <thijsvandiessen@gmail.com>
2026-05-19 12:11:31 +0200

    fix: csp update

--- packages/next-js/src/config/cspConfig.ts
+++ packages/next-js/src/config/cspConfig.ts
@@ -7,7 +7,7 @@ export const csp = ({ domain }: Props) => [
   `script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com`,
   `manifest-src ${domain}/manifest.json`, // TODO
   `img-src 'self' 'unsafe-inline' data: https://maps.gstatic.com https://maps.googleapis.com https://i.ytimg.com https://i.vimeocdn.com https://res.cloudinary.com`,
-  `connect-src 'self' https://maps.googleapis.com https://j3j4j0es2k-dsn.algolia.net`,
+  `connect-src 'self' https://maps.googleapis.com https://j3j4j0es2k-dsn.algolia.net https://graphql.datocms.com/`,
   `script-src-elem 'self' 'unsafe-inline' https://static.cloudflareinsights.com https://maps.googleapis.com`,
   `style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com`,
   `font-src 'self' https://fonts.gstatic.com`,
